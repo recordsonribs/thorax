26f273fe6b23fb6e2c911d6cecfb5d6f40a98ef8
Alex <alex@recordsonribs.com>
2014-06-29 17:52:22 +0100

    WordPress plugin scaffolding.

---
+++ thorax.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: thorax
+Version: 0.1-alpha
+Description: The new version of our record label management software.
+Author: Alex Andrews <alex@recordsonribs.com>
+Author URI: http://recordsonribs.com
+Plugin URI: http://recordsonribs.com/thorax
+Text Domain: thorax
+Domain Path: /languages
+*/
+
