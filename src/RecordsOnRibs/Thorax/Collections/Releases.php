d5311b7782c2fdd9ca8b58dee7aa4609716b0a1d
Alex <alex@recordsonribs.com>
2014-09-14 19:15:29 +0100

    Add releases. #6

---
+++ src/RecordsOnRibs/Thorax/Collections/Releases.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace RecordsOnRibs\Thorax\Collections;
+
+use RecordsOnRibs\Thorax\Collection as Collection;
+
+class Releases extends Collection { }
--- src/RecordsOnRibs/Thorax/Thorax.php
+++ src/RecordsOnRibs/Thorax/Thorax.php
@@ -3,6 +3,7 @@
 namespace RecordsOnRibs\Thorax;
 
 use RecordsOnRibs\Thorax\Collections\Artists as Artists;
+use RecordsOnRibs\Thorax\Collections\Releases as Releases;
 
 class Thorax
 {
@@ -13,6 +14,7 @@ class Thorax
 
    	public function initialise()
    	{
-   		$this->artists = new Artists(['parent' => true]);
+   		$this->artists = new Artists( [ 'parent' => true] );
+   		$this->releases = new Releases( [ 'parent' => true ] );
    	}
 }
