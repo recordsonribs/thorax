ec3c97f9fefa582c2d3cbf9dd6e88128f3dd9b3e
Alex <alex@recordsonribs.com>
2015-02-23 14:47:08 +0000

    Complete support for basic artist details. Closes #3.

--- src/RecordsOnRibs/Thorax/Collections/Artists.php
+++ src/RecordsOnRibs/Thorax/Collections/Artists.php
@@ -73,6 +73,36 @@ class Artists extends Collection {
 			]
 		];
 
+		$metaboxes['press_information'] = [
+			'id'            => $prefix . 'urls',
+			'title'         => 'Press Information',
+			'object_types'  => [ $this->post_type, ],
+			'context'       => 'normal',
+			'priority' => 'high',
+			'fields' => [
+				[
+				    'name' => 'Press Photo',
+				    'desc' => 'A high-quality image, of sufficient quality for print publication',
+				    'id' => $prefix . 'press_image',
+				    'type' => 'file',
+				    'options' => array(
+        				'add_upload_file_text' => 'Upload JPEG',
+    				),
+				    'allow' => [ 'attachment' ]
+				],
+				[
+				    'name' => 'Press One Sheet',
+				    'desc' => 'A PDF containing information about the artist',
+				    'id' => $prefix . 'press_image',
+				    'type' => 'file',
+				   	'options' => [
+        				'add_upload_file_text' => 'Upload PDF',
+    				],
+				    'allow' => [ 'attachment' ]
+				]
+			]
+		];
+
 		return $metaboxes;
 	}
 }
