7c349b14638104ae12de79cfbfd287d3140f4b0d
Alex <alex@recordsonribs.com>
2015-02-23 19:22:06 +0000

    Add metaboxes to release for tracks.

--- src/RecordsOnRibs/Thorax/Collections/Releases.php
+++ src/RecordsOnRibs/Thorax/Collections/Releases.php
@@ -6,6 +6,46 @@ use RecordsOnRibs\Thorax\Collection as Collection;
 
 class Releases extends Collection { 
 	function __construct() {
-		parent::__construct( [ 'parent' => true ] );
+		$metaboxes = [];
+
+		$prefix = '_release_';
+
+		$this->post_type = 'release';
+
+		$tracks = [
+			'id'          => $prefix . 'track_listing',
+			'type'        => 'group',
+			'options'     => [
+			    'group_title'   => 'Track {#}',
+			    'add_button'    => 'Add Another Track',
+			    'remove_button' => 'Remove Track',
+			    'sortable'      => true, 
+			],
+			'fields'      => [
+			    [
+			        'name' => 'Track Title',
+			        'id'   => 'title',
+			        'type' => 'text'
+			    ],
+				[
+				    'name' => 'Time',
+				    'id' => 'time',
+				    'type' => 'text_small'
+				]
+			],
+		];
+
+		array_push($metaboxes, [
+			'id'            => $prefix . 'urls',
+			'title'         => 'Tracks',
+			'object_types'  => [ $this->post_type ],
+			'context'       => 'normal',
+			'priority' => 'low',
+			'fields' => [
+				$tracks
+			]
+		]);
+
+		parent::__construct( [ 'parent' => true, 'metaboxes' => $metaboxes ] );
 	}
 }
